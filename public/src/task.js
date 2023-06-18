2d983b3ab87527b1616606c17a9b78afd7a58133
Sepehr Saeedpour <sepehr@Sepehrs-MacBook-Air.local>
2023-06-19 00:17:30 +0330

    Persian instruction for group is added.

--- public/src/task.js
+++ public/src/task.js
@@ -3,8 +3,8 @@ var timeline = [];
 // full screen mode
 
 // timeline.push(consent);
-timeline.push(consent_first_page);
-timeline.push(consent_second_page);
+// timeline.push(consent_first_page);
+// timeline.push(consent_second_page);
 
 
 
@@ -18,17 +18,17 @@ timeline.push(first_welcome_page);
 
 // timeline.push(fullscreen)
 
-timeline.push(before_instructions);
+// timeline.push(before_instructions);
 
 timeline.push(instructions);
 
 //  instruction Questions
-timeline.push(instruction_questions);
+// timeline.push(instruction_questions);
 
-timeline.push(instruction_question_feedback);
+// timeline.push(instruction_question_feedback);
 
 // instructuon Questions
-timeline.push(instructions_repeat);
+// timeline.push(instructions_repeat);
 
 // practice code
 timeline.push(practice_instruction);
@@ -85,7 +85,7 @@ jsPsych.data.addProperties({
 // initialize experiment
 jsPsych.init({
   timeline: timeline,
-  on_interaction_data_update: on_interaction_data_update,
+  // on_interaction_data_update: on_interaction_data_update,
   show_progress_bar: true,
   exclusions: {
     min_width: 600,
